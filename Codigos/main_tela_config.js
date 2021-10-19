ad21c3ba7ba81b4b0c01c2101c12d1afe83485a3
pabloaugusto17 <pablo.magalhaes@sga.pucmail.br>
2021-10-19 17:15:41 -0300

    UPDATE Codigos

---
+++ Codigos/main_tela_config.js
@@ -0,0 +1,17 @@
+function define_atributos_config(){
+
+    var nome_config = "Pablo";
+    nome_usuario.textContent = nome_config;
+
+    var data_nascimento_usuario = "17/05/2003";
+    data_nascimento.textContent = data_nascimento_usuario;
+
+    var email = "pabloaugustocm@gmail.com";
+    email_usuario.textContent = email;
+
+    var telefone = "31982378197";
+    telefone_usuario.textContent = telefone;
+}
+
+define_atributos_config();
+
